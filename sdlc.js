c2032504b0d6f731d96b0d434922651059fbe608
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-02-23 10:08:27 -0800

    problem 2

--- sdlc.js
+++ sdlc.js
@@ -24,4 +24,15 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
+
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisRequirements = "In this step we define what is required to implement a new project or feature."
+
+const design = "In the design stage programmers will create the architecture for the project they are developing."
+
+const implementation = "Implementation is the process of writing the code that will be used to run a program."
+
+const testingIntegration = "In this phase devops will test the code to try and find potential bugs. This allows the developers to resolve issues."
+
+const maintain = "Mainting the code to improve performance, fix bugs, add new features."
